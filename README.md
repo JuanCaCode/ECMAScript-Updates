1e709e8b91ad9e15c34bbc957f056ed981bc1805
Juan Camilo Campo <juanccampo95@gmail.com>
2022-12-08 16:01:04 -0500

    actualización 10-set-add.js & actualizacion de imagen en readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ECMAScript-Updates
 In this repository you will find several ECMAScript Updates, since ES6 to ES13
+
+![](https://blog.ida.cl/wp-content/uploads/sites/5/2016/06/ecmas6_2.png)
---
+++ src/es6/10-set-add.js
@@ -0,0 +1,9 @@
+// instanciamos un Set
+const list = new Set()
+
+list.add('item 1')
+// Aquí se puede encadenar
+list.add('item 2').add('item 3')
+
+console.log(list)
+console.log(typeof(list))
